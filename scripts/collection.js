123abb0448c861784650785936c599476d8c2eef
Alex Lewis <alew46@gmail.com>
2017-05-04 11:56:25 -0400

    Checkpoint 24: completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -21,6 +21,6 @@ var collectionItemTemplate =
      collectionContainer.innerHTML = '';
  
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+         collectionContainer.innerHTML += collectionItemTemplate; 
      }
  }
